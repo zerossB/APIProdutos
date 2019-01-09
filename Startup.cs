352440036e2ba56f4e8cf1df338f2520bd22c468
Guilherme Haynes <zerossb@gmail.com>
2019-01-09 13:37:27 -0200

    Edit: Configurando Serviços (Produto, Usuário) na Startup da Aplicação

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Microsoft.EntityFrameworkCore;
+using APIProdutos.Models;
+using APIProdutos.Data;
+using APIProdutos.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 namespace APIProdutos
 {
     public class Startup
@@ -25,12 +32,23 @@ namespace APIProdutos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("InMemoryDatabase"));
+            services.AddScoped<ProdutoService>();
+            services.AddScoped<UsuarioService>();
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, UsuarioService usrService)
         {
+            usrService.Incluir(
+                new Usuario() { ID = "usuario01", ChaveAcesso = "94be650011cf412ca906fc335f615cdc" });
+            usrService.Incluir(
+                new Usuario() { ID = "usuario02", ChaveAcesso = "531fd5b19d58438da0fd9afface43b3c" });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
